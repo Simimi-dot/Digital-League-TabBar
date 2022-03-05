557b9d47ce95060ec11113dc4f25e8ba682df65c
Egor Astakhov <noise718@gmail.com>
2022-03-05 18:40:38 +0300

    Auto Layout controller

--- HomeWorkTabBar/Auto Layout/AutoLayoutContoller.swift
+++ HomeWorkTabBar/Auto Layout/AutoLayoutContoller.swift
@@ -10,21 +10,73 @@ import UIKit
 
 //MARK: - Class
 class AutoLayutContoller: UIViewController {
-
-//MARK: - Properties
-    let customView = CustomView()
-
-//MARK: - Life Cycle
+    
+    //MARK: - Properties
+    let customView = CustomView(courseEvent: .backend, courseName: .backend, coursePrice: .backend)
+    
+    //MARK: - Life Cycle
     override func loadView() {
         self.view = customView
-    
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configure()
         
         view.backgroundColor = .systemRed
     }
+    
+    //MARK: - Methods
+    
+    func configure() {
+        self.view.backgroundColor = .green
+        
+        self.view.addSubview(customView.customCourseView)
+        
+        customView.customCourseView.addSubview(customView.courseEventLabel)
+        customView.customCourseView.addSubview(customView.backEventView)
+        customView.customCourseView.addSubview(customView.courseNameLabel)
+        customView.customCourseView.addSubview(customView.moreButton)
+        customView.customCourseView.addSubview(customView.priceLabel)
+        customView.customCourseView.addSubview(customView.blueButton)
+        
+        NSLayoutConstraint.activate([
+            customView.customCourseView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            customView.customCourseView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            customView.customCourseView.widthAnchor.constraint(equalToConstant: 343),
+            customView.customCourseView.heightAnchor.constraint(equalToConstant: 174),
+            
+            customView.courseEventLabel.topAnchor.constraint(equalTo: customView.customCourseView.topAnchor, constant: 24),
+            customView.courseEventLabel.leadingAnchor.constraint(equalTo: customView.customCourseView.leadingAnchor, constant: 32),
+            customView.courseEventLabel.trailingAnchor.constraint(lessThanOrEqualTo: customView.customCourseView.trailingAnchor, constant: -20),
+            customView.courseEventLabel.bottomAnchor.constraint(equalTo: customView.customCourseView.bottomAnchor, constant: -134),
+            
+            customView.backEventView.topAnchor.constraint(equalTo: customView.customCourseView.topAnchor, constant: 20),
+            customView.backEventView.leadingAnchor.constraint(equalTo: customView.customCourseView.leadingAnchor, constant: 20),
+            customView.backEventView.trailingAnchor.constraint(greaterThanOrEqualTo: customView.courseEventLabel.trailingAnchor, constant: 12),
+            customView.backEventView.bottomAnchor.constraint(equalTo: customView.courseNameLabel.topAnchor, constant: -12),
+            
+            customView.courseNameLabel.topAnchor.constraint(lessThanOrEqualTo: customView.customCourseView.topAnchor, constant: 56),
+            customView.courseNameLabel.leadingAnchor.constraint(equalTo: customView.customCourseView.leadingAnchor, constant: 20),
+            customView.courseNameLabel.trailingAnchor.constraint(equalTo: customView.customCourseView.trailingAnchor, constant: -20),
+            customView.courseNameLabel.bottomAnchor.constraint(equalTo: customView.customCourseView.bottomAnchor, constant: -96),
+            
+            customView.moreButton.topAnchor.constraint(equalTo: customView.courseNameLabel.bottomAnchor, constant: 4),
+            customView.moreButton.leadingAnchor.constraint(equalTo: customView.customCourseView.leadingAnchor, constant: 20),
+            customView.moreButton.trailingAnchor.constraint(equalTo: customView.customCourseView.trailingAnchor, constant: -244),
+            customView.moreButton.bottomAnchor.constraint(equalTo: customView.customCourseView.bottomAnchor, constant: -72),
+            
+            customView.priceLabel.topAnchor.constraint(equalTo: customView.moreButton.bottomAnchor, constant: 23),
+            customView.priceLabel.leadingAnchor.constraint(equalTo: customView.customCourseView.leadingAnchor, constant: 20),
+            customView.priceLabel.trailingAnchor.constraint(equalTo: customView.customCourseView.trailingAnchor, constant: -257),
+            customView.priceLabel.bottomAnchor.constraint(equalTo: customView.customCourseView.bottomAnchor, constant: -27),
+            
+            customView.blueButton.topAnchor.constraint(equalTo: customView.customCourseView.topAnchor, constant: 118),
+            customView.blueButton.leadingAnchor.constraint(equalTo: customView.customCourseView.leadingAnchor, constant: 251),
+            customView.blueButton.trailingAnchor.constraint(equalTo: customView.customCourseView.trailingAnchor, constant: -20),
+            customView.blueButton.bottomAnchor.constraint(equalTo: customView.customCourseView.bottomAnchor, constant: -20)
+        ])
+    }
+    
     
 }
--- HomeWorkTabBar/CustomView.swift
+++ HomeWorkTabBar/CustomView.swift
@@ -7,97 +7,153 @@
 
 import UIKit
 
+//MARK: - Enum
+
+enum courseName: String {
+    case backend = "Backend разработка"
+    case andriod = "Курсы по Android разработкe"
+    case ios = "Курсы по iOS разработке + Computer Since + Computer Vision + System Design"
+}
+
+enum coursePrise: String {
+    case backend = "7840 Р"
+    case android = "9800 Р"
+    case ios = "11760 Р"
+}
+
+enum courseEvent: String {
+    case backend = "Первые три занятия бесплатно"
+    case android = "1.02.2022 - 1.04.2022 Скидка 10%"
+}
+
 //MARK: - Class
+
 class CustomView: UIView {
     
     //MARK: - Properties
     
-    var customTableView: UITableView = {
-        var tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        return tableView
+    lazy var customCourseView: UIView = {
+        var view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 10
+        view.backgroundColor = .white
+        return view
     }()
     
-    lazy var someBlueButton: UIButton = {
+    lazy var courseEventLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .lightGray
+        label.font = UIFont.systemFont(ofSize: 14, weight: .light)
+        return label
+    }()
+    
+    lazy var backEventView: UIView = {
+        var view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = 12
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.lightGray.cgColor
+        return view
+    }()
+    
+    lazy var courseNameLabel: UILabel = {
+        var name = UILabel()
+        name.translatesAutoresizingMaskIntoConstraints = false
+        name.textColor = .black
+        name.numberOfLines = 3
+        return name
+    }()
+    
+    lazy var moreButton: UIButton = {
+        var button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Подробнее", for: .normal)
+        button.setTitleColor(.darkGray, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .light)
+        button.backgroundColor = .clear
+        return button
+    }()
+    
+    lazy var priceLabel: UILabel = {
+        var priceLabel = UILabel()
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        priceLabel.textColor = .black
+        priceLabel.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+        return priceLabel
+    }()
+    
+    lazy var blueButton: UIButton = {
         var button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("+", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 25, weight: .bold)
-        button.backgroundColor = .systemBlue
         button.setTitleColor(.white, for: .normal)
-        button.layer.cornerRadius = 10
-        button.addTarget(self, action: #selector(someBlueButtonPush), for: .touchUpInside)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 35, weight: .light)
+        button.backgroundColor = .systemBlue
         return button
     }()
     
     
     // MARK: - init
     
-    init() {
+    init(courseEvent: courseEvent?, courseName: courseName, coursePrice: coursePrise) {
         super.init(frame: .zero)
         
-        configure()
-        setTableViewDelegate()
+        if let courseEvent = courseEvent {
+            self.courseEventLabel.text = courseEvent.rawValue
+        }
         
+        self.courseNameLabel.text = courseName.rawValue
+        self.priceLabel.text = coursePrice.rawValue
+        //        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
     // MARK: - Methods
     
-    func configure() {
-        self.addSubview(customTableView)
-        customTableView.addSubview(someBlueButton)
-        self.customTableView.register(CustomCell.self, forCellReuseIdentifier: "MyCustomCell")
-        
-        NSLayoutConstraint.activate([
-            customTableView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
-            customTableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            customTableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            customTableView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            
-            someBlueButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 118),
-            someBlueButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 251),
-            someBlueButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-            someBlueButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20)
-            
-        ])
-        
-    }
+    //    private func configure() {
+    //        self.backgroundColor = .white
+    //
+    //        self.addSubview(customCourseView)
+    //
+    //        customView.addSubview(courseNameLabel)
+    //        customView.addSubview(moreButton)
+    //        customView.addSubview(priceLabel)
+    //        customView.addSubview(blueButton)
+    //
+    //        NSLayoutConstraint.activate([
+    //            customView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 50),
+    //            customView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+    //            customView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+    //
+    //            courseNameLabel.topAnchor.constraint(equalTo: customView.topAnchor, constant: 16),
+    //            courseNameLabel.leadingAnchor.constraint(equalTo: customView.leadingAnchor, constant: 16),
+    //            courseNameLabel.trailingAnchor.constraint(equalTo: customView.trailingAnchor, constant: -16),
+    //
+    //            moreButton.topAnchor.constraint(equalTo: courseNameLabel.bottomAnchor, constant: 16),
+    //            moreButton.leadingAnchor.constraint(equalTo: customView.leadingAnchor, constant: 16),
+    //            moreButton.trailingAnchor.constraint(equalTo: customView.trailingAnchor, constant: -16),
+    //
+    //            priceLabel.topAnchor.constraint(equalTo: moreButton.bottomAnchor, constant: 16),
+    //            priceLabel.leadingAnchor.constraint(equalTo: customView.leadingAnchor, constant: 16),
+    //            priceLabel.trailingAnchor.constraint(equalTo: customView.trailingAnchor, constant: -16),
+    //
+    //            blueButton.topAnchor.constraint(equalTo: priceLabel.bottomAnchor, constant: 16),
+    //            blueButton.leadingAnchor.constraint(equalTo: customView.leadingAnchor, constant: 16),
+    //            blueButton.trailingAnchor.constraint(equalTo: customView.trailingAnchor, constant: -16),
+    //            blueButton.bottomAnchor.constraint(equalTo: customView.bottomAnchor, constant: -16)
+    //
+    //        ])
+    //    }
     
-    func setTableViewDelegate() {
-        customTableView.delegate = self
-        customTableView.dataSource = self
-        
-    }
     
 }
 
 //MARK: - Extension
-
-extension CustomView: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCustomCell") as! CustomCell
-        cell.customBlueButton = self.someBlueButton
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 174
-    }
-    
-    @objc
-    func someBlueButtonPush() {
-        print("Hi")
-    }
-    
-}
-
 
